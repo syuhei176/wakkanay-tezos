d0cd4dc1e3708de4c01597857b3214a137728592
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-08 13:39:25 +0900

    Fix spec #43

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -2,6 +2,8 @@ import { EthWallet } from './eth/EthWallet'
 import { EthWalletFactory } from './eth/EthWalletFactory'
 export * from './interfaces/IWallet'
 export * from './interfaces/IWalletFactory'
+export * from './eth/EthWallet'
+export * from './eth/EthWalletFactory'
 
 // TODO: switch by env
 const Wallet = EthWallet
