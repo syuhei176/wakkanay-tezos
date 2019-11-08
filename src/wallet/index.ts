351a9a550be7da7ffd996b0f666bb0007700fdcf
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-08 13:11:42 +0900

    Add export default in wallet/index.ts

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -1,4 +1,9 @@
-export * from './eth/EthWallet'
-export * from './eth/EthWalletFactory'
+import { EthWallet } from './eth/EthWallet'
+import { EthWalletFactory } from './eth/EthWalletFactory'
 export * from './interfaces/IWallet'
 export * from './interfaces/IWalletFactory'
+
+// TODO: switch by env
+const Wallet = EthWallet
+const WalletFactory = EthWalletFactory
+export default { Wallet, WalletFactory }
