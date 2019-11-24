f06d4a449e7e4efde50cb718139f215e46030ed4
daiki.sekiguchi <sekiseki0213@gmail.com>
2019-11-25 02:04:33 +0900

    Fix contract parameter

--- src/contract/CommitmentContract.ts
+++ src/contract/CommitmentContract.ts
@@ -12,7 +12,8 @@ export class CommitmentContract implements ICommitmentContract {
   async submit(blockNumber: number, root: Bytes) {
     const amount = 0
     const entrypoint = 'submit'
-    const params = `(Right (Pair (Pair ${blockNumber} "${blockNumber.toString()}") "${root.toHexString()}"))`
+    const params = `(Pair (Pair ${blockNumber} "${blockNumber.toString()}") "${root.toHexString()}")`
+
     await this.connection.invokeContract(amount, entrypoint, params)
   }
 
